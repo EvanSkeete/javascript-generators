e3db88c2975f5b00aafc23e916a1e8a320de2079
Evan Skeete <evanskeete@gmail.com>
2016-11-07 19:00:51 -0500

    Done some stuff

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "google",
+    "parserOptions": {
+      "ecmaVersion": 8
+    },
+    "installedESLint": true,
+    "rules": {
+      "require-jsdoc": 0,
+    }
+};
--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+import "babel-polyfill";
+import api from './lib/api.js';
+import cache from './lib/cache.js';
+import {
+  printPokemonInfo,
+  printPokemonInfo2,
+  printPokemonInfo3
+} from './lib/generators.js';
+
+// api.get('pokemon', '1', console.log);
+
 const repl = require('repl');
-var msg = 'message';
-repl.start('>>> ');
+const r = repl.start('>>> ');
+r.context.api = api;
+r.context.cache = cache;
+r.context.printPokemonInfo = printPokemonInfo;
+r.context.printPokemonInfo2 = printPokemonInfo2;
+r.context.printPokemonInfo3 = printPokemonInfo3;
